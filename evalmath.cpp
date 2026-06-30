daa27bd0aba44b24bdd37e2f8c327228822fbcec
ribbon-otter <ribbon-otter@github.invalid>
2026-06-30 13:13:50 -0700

    ready for deployment

--- evalmath.cpp
+++ evalmath.cpp
@@ -11,17 +11,33 @@
 #include <format>
 #include <print>
 
+#include "evalmath.h"
+
+// uncomment to enable print statements and asserts helpful for development only
+// #define EVALMATH_DEBUG 
+#ifdef EVALMATH_DEBUG
+    #define EVALMATH_PRINTLN(...) std::println( __VA_ARGS__)
+    #define EVALMATH_PRINT(...) std::print(__VA_ARGS__)
+    #define EVALMATH_ASSERT(...) assert( __VA_ARGS__)
+    #define EVALMATH_PRINT_STACK(stack,tag) print_stack((stack), (tag))
+#else
+    #define EVALMATH_PRINTLN(...)
+    #define EVALMATH_PRINT(...)
+    #define EVALMATH_ASSERT(...)
+    #define EVALMATH_PRINT_STACK(stack,tag)
+#endif
+
 using size_t = std::size_t;
 
 namespace evalmath {
     namespace details {
-        bool is_digit (char a) {
+        bool static is_digit (char a) {
             return '0' <= a && a <= '9';
         }
-        bool is_whitespace (char a) {
+        bool static is_whitespace (char a) {
             return ' ' == a;
         }
-        bool is_operator (char a) {
+        bool static is_operator (char a) {
             return a == '+'
                 || a == '*'
                 || a == '-'
@@ -36,9 +52,8 @@ namespace evalmath {
             END_OF_TEXT
 
         };
-
-        [[maybe_unused]]
-        std::string_view to_sv(const token_type& a) {
+#ifdef EVALMATH_DEBUG
+        std::string_view static to_sv(const token_type& a) {
             switch (a) {
                 case token_type::NUMBER: return "{num}";
                 case token_type::OPERATOR: return "{op}";
@@ -46,9 +61,10 @@ namespace evalmath {
                 case token_type::LEFT_PARENTHESES: return "{ ( }";
                 case token_type::INVALID: return "{invalid}";
                 case token_type::END_OF_TEXT: return "{EOT}";
-                default: return "{unkown token type}";
+                default: return "{unknown token type}";
             }
         }
+#endif
         
         struct token {
             token_type type;
@@ -59,19 +75,26 @@ namespace evalmath {
             };
         };
         
-        double parse_num(std::string_view num_str) {
+        double static parse_num(std::string_view num_str) {
+#ifdef __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wunsafe-buffer-usage"
+#endif
             double value = 0;
             auto [ptr, ec] =
                 std::from_chars(num_str.data(), num_str.data() + num_str.size(), value);
-            ////std::println("parsing number '{}' ", num_str);
+            EVALMATH_PRINTLN("\tparsing number '{}' ", num_str);
             assert(ec == std::errc()); //there should never be an error
             assert(ptr == num_str.data() + num_str.size()); //and we use up the entire 
                                                             //token value
+#ifdef __clang__
+#pragma clang diagnostic pop
+#endif
             return value;
         }
         
         ///returns a token if there are tokens left
-        std::pair<std::optional<token>, size_t> next_token(std::string_view text) {
+        std::pair<std::optional<token>, size_t> static next_token(std::string_view text) {
             if (text.empty()) return {};
             auto all_start = text.begin(); //text including whitespace
             while (!text.empty() && is_whitespace(text.front())) text.remove_prefix(1);
@@ -85,35 +108,29 @@ namespace evalmath {
                 //we are a number starting with .
                 type = token_type::NUMBER;
                 while (!text.empty() && is_digit(text.front())) {
-                    //std::print("{}", text.front());
                     text.remove_prefix(1);
                 }
             } else if (is_digit(start_char) 
-                || start_char == '-' && !text.empty() && is_digit(text.front())
+                || ( start_char == '-' && !text.empty() && is_digit(text.front()) )
                 //we will assume we are a negative number in the case of '-'
                 //and then in the reducer we will promote negative numbers into
                 //'-' operators if needed
                 ) { // parse digit
                 type = token_type::NUMBER;
-                //std::print("new number token! '{}", start_char);
                 while (!text.empty() && is_digit(text.front())) {
-                    //std::print("{}", text.front());
                     text.remove_prefix(1);
                 }
                 if (!text.empty() && text.front() == '.') {
-                    //std::print("{}", text.front());
                     text.remove_prefix(1);
                 }
                 while (!text.empty() && is_digit(text.front())) {
-                    //std::print("{}", text.front());
                     text.remove_prefix(1);
                 }
-                ////std::println("'");
             } else if (is_operator(start_char)) { // parse operator
                 type = token_type::OPERATOR;
             } else if ('(' == start_char) { // parse left parentheses
                 type = token_type::LEFT_PARENTHESES;
-            } else if (')' == start_char) { // parse right parenthese
+            } else if (')' == start_char) { // parse right parentheses
                 type = token_type::RIGHT_PARENTHESES;
             } else {
                 type = token_type::INVALID;
@@ -123,7 +140,7 @@ namespace evalmath {
             }
             auto end = text.begin();//the end of where we stopped gobbling
             //how much the caller need to advance their head
-            size_t len = end - all_start; 
+            long len = end - all_start; 
             switch (type) {
                 case token_type::NUMBER:
                     return 
@@ -131,30 +148,35 @@ namespace evalmath {
                 case token_type::OPERATOR:
                     return {token{.type = type, .value_char = start_char}, len};
                 case token_type::LEFT_PARENTHESES:
-                    return {token{type}, len};
+                    //we don't care about the value (we just suppress the warning)
+                    return {token{type, {0.0}}, len}; 
                 case token_type::RIGHT_PARENTHESES:
-                    return {token{type}, len};
+                    //we don't care about the value (we just suppress the warning)
+                    return {token{type, {0.0}}, len};
                 case token_type::END_OF_TEXT:
-                case token_type::INVALID:
+                case token_type::INVALID: {
                     auto sv = std::string_view{token_start, end};
                     return {token{.type = type, .value_sv=sv}, len};
+                }
+                default:
+                    assert(false); //should be unreachable
             }
-            assert(false);
+            assert(false); //should be unreachable
         }
         
-        int op_precedence(char op) {
+        int static op_precedence(char op) {
             if (op == '+' || op == '-') return 1;
             if (op == '*' || op == '/') return 2;
             assert(false);
             return 0;
         }
 
-        int op_precedence(token op) {
+        int static op_precedence(token op) {
             assert(op.type == token_type::OPERATOR);
             return op_precedence(op.value_char);
         }
  
-        double eval_op(double lhs, char op, double rhs) {
+        double static eval_op(double lhs, char op, double rhs) {
             if (op == '+') {
                 return lhs + rhs;
             }
@@ -167,32 +189,32 @@ namespace evalmath {
             if (op == '/') {
                 return lhs / rhs;
             }
-            ////std::println("foo '{}'", op);
             assert(false); //we shouldn't produce tokens with ops
                            //that aren't handled here
             return 0;
         }
+
+#ifdef EVALMATH_DEBUG
+        void static print_stack(std::vector<token> &stack, std::string_view tag) {
+            for (const auto& i : stack) {std::print("{},", to_sv(i.type));}
+            std::println("<- {}", tag);
+        }
+#endif
  
         ///the reduce half of the shift reduce parser
-        ///returns a value only if an error occured
+        ///returns a value only if an error occurred
 
-        //TODO the bug is caused by skipping the num,op,num,op case
-        //because we go directly from num,op,num,num
-        //to num,op,num,op,num when evaluating 6-2-3 (and thus calc 6-(2-3))
-        //incorrectly
-
-        std::optional<std::string>
+        std::optional<std::string> static
         reduce(std::vector<token> &stack) {
-//            for (const auto& i : stack) {std::print("{},", to_sv(i.type));}
-            ////std::println("<- stack at start");
+            EVALMATH_PRINTLN("----- Begin reduce operation");
             
-            //itterations of the inner loop are written assuming that 
+            //iterations of the inner loop are written assuming that 
             //assuming other patterns don't add tokens to the stack
-            //and all patterns have an oppotunity to match each 
+            //and all patterns have an opportunity to match each 
             //additional token
 
             //however some patterns do need to add tokens onto the stack
-            //(for example, the one to disambiguate 3-4 (with an opperator -)
+            //(for example, the one to disambiguate 3-4 (with an operator -)
             //and 3 (-4)  (3 next to -4)
             //so queues hold such generated tokens so the other patterns can match
             // on the in between steps
@@ -204,8 +226,8 @@ namespace evalmath {
                     queue.pop_back();
                 }
                 while (!stack.empty()) {
-                    //for (const auto& i : stack) {//std::print("{},", to_sv(i.type));}
-                    ////std::println("<- stack this round");
+                    EVALMATH_PRINT_STACK(stack, "<- stack at start of matching");
+                    
                     const auto& a = stack[stack.size() - 1];
                     if (a.type == token_type::INVALID) {
                         return std::format("invalid token: '{}'", a.value_sv);
@@ -224,10 +246,9 @@ namespace evalmath {
                             //actually the negative number is a minus operator
                             
                             //NOTE: we have to error on '3(' earlier to prevent 
-                            //interpeting 3(-4) as 3-4
-                            //for (const auto& i : stack) {//std::print("{},", to_sv(i.type));}
-                            double new_value = -a.value_num;
-                            ////std::println("<- stack before transmorg ({})", new_value);
+                            //interpreting 3(-4) as 3-4
+                            EVALMATH_PRINT_STACK(stack,
+                                "stack before '-' operator transmorg");
                             token pos_val{.type=token_type::NUMBER, 
                                           .value_num= -a.value_num};
                             stack.pop_back();
@@ -236,8 +257,7 @@ namespace evalmath {
                             //we are making the stack longer,
                             //so we need to use the queue to dribble the elements on it
                             queue.emplace_back(pos_val);
-                            //for (const auto& i : stack) {//std::print("{},", to_sv(i.type));}
-                            ////std::println("<- stack after transmorg");
+                            EVALMATH_PRINT_STACK(stack, "stack after '-' operator transmorg");
                             continue; //invalidated references, so we start again
                             
                         } else {
@@ -306,44 +326,44 @@ namespace evalmath {
                     //no risk that a higher priority operator can show up later
                     //since we are at the end of the text
                     if (
-                        (   a.type == token_type::END_OF_TEXT
+                        ((   a.type == token_type::END_OF_TEXT
                             || a.type == token_type::RIGHT_PARENTHESES
                             )
                         && b.type == token_type::NUMBER
                         && c.type == token_type::OPERATOR
-                        && d.type == token_type::NUMBER
+                        && d.type == token_type::NUMBER)
                         
                         ||
 
-                        a.type == token_type::OPERATOR
+                        (a.type == token_type::OPERATOR
                         && b.type == token_type::NUMBER
                         && c.type == token_type::OPERATOR
                         && d.type == token_type::NUMBER
-                        && op_precedence(a) <= op_precedence(c)
+                        && op_precedence(a) <= op_precedence(c))
                         
                         ) {
                         token end = a; //remember the end so we can put it back
                         double value_num = eval_op(d.value_num,c.value_char,b.value_num); 
                         
+                        EVALMATH_PRINT("evaluated {} {} {}", 
+                            d.value_num, c.value_char, b.value_char);
                         stack.pop_back();
                         stack.pop_back();
                         stack.pop_back();
                         stack.pop_back();
-                        stack.emplace_back(token{token_type::NUMBER, value_num});
+                        stack.emplace_back(
+                            token{token_type::NUMBER, {.value_num=value_num} });
                         stack.emplace_back(end);
-                        ////std::println("calculated {}", value_num);
+                        EVALMATH_PRINTLN("as {}", value_num);
                         continue; //reinitialize the a,b,c,d references 
                                   //since they are officially invalid now
-                    } else {
-                        /* for (const auto& i : stack) {//std::print("{},", to_sv(i.type));} */
-                        /* ////std::println("<- stack at end"); */
-                        /* ////std::println("{} {} {} {} --", to_sv(d.type), to_sv(c.type), */
-                        /*                             to_sv(b.type), to_sv(a.type)); */
-                        
-                    }
+                    } 
+                    EVALMATH_PRINT_STACK(stack, "stack at end of match");
+                    EVALMATH_PRINT_STACK(queue, "queue at end of match");
                     break;
                 }
             } while (!queue.empty());
+            EVALMATH_PRINTLN("----- reduce operation ended");
             return {};
         }
     }
@@ -360,18 +380,23 @@ namespace evalmath {
             stack.push_back(this_token);
             if (auto maybe_error = reduce(stack)) return std::unexpected(*maybe_error);
         }
-        stack.push_back(token{token_type::END_OF_TEXT});
+        //END_OF_TEXT tokens don't use value, but we supply a 0.0 suppress warnings
+        stack.push_back(token{token_type::END_OF_TEXT, {.value_num=0.0} }); 
         if (auto maybe_error = reduce(stack)) return std::unexpected(*maybe_error);
-        assert(stack.size() == 1);
-        assert(stack.front().type == token_type::NUMBER);
+        
+        ///I should add a particular match to reduce to catch the invalid formula
+        ///before these hit, but we shouldn't crash if it somehow happens in production
+        EVALMATH_ASSERT(stack.size() == 1);
+        EVALMATH_ASSERT(stack.front().type == token_type::NUMBER);
         if (stack.size() != 1 && stack.front().type == token_type::NUMBER) { 
             //one of our better error detecting rules didn't find
             //the specific error inside reduce
             return std::unexpected(std::format("invalid formula '{}'", formula));
         }
-        ////std::println("==");
+        EVALMATH_PRINTLN("eval returns -- {} evaluated as {}", 
+            formula, stack.front().value_num);
         return stack.front().value_num;
-    };
+    }
 }
 
 // vim: ts=4 sw=4 expandtab
